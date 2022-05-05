35b0d2246dbaa6e7eb14f7eb455430d678019c3f
LexieHolder <lexieannholder@gmail.com>
2022-05-05 12:49:57 -0400

    added contact form

---
+++ src/components/Contact/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function ContactForm() {
+    //JSX
+
+    return (
+        <section>
+            <h1>Contact Me</h1>
+            <form id="contact-form">
+                //name input
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" />
+                </div>
+                //email input
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" name="email" />
+                </div>
+                //message text input area
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" rows="5" />
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </section>
+    )
+
+
+}
+
+export default ContactForm;
